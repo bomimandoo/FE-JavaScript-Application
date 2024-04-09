7f461bfd1649c5317283b95a893e28a00f5c10ef
bomimandoo <163390823+bomimandoo@users.noreply.github.com>
2024-04-09 23:12:52 +0900

    Update Problem6.js

--- Problem6.js
+++ Problem6.js
@@ -5,13 +5,15 @@ const arr = [{ part: "fe", name: "홍현", age: 26},
   { part: "fe", name: "나연", age: 23},
   { part: "staff", name: "나현", age: 24}];
 
+
   const filtered =arr.filter((babylion) => babylion.part ==="fe");
   console.log(filtered);
 
-  const age = [23,24,26];
-  age.sort(function(a,b){return b-a});
+
+  //수정
+  const age= filtered.sort(function(a,b){return b-a});
   console.log(age);
 
-  
+
   const key = filtered.map(key => ({ ...key, part: key.part.toUpperCase()}));
   console.log(key);
